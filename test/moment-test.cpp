a2230d52ac8d69a736dff3a3b8e112aaa44bdaf3
builden <billdeng@qq.com>
2016-06-25 19:20:00 +0800

    add moment test

--- test/moment-test.cpp
+++ test/moment-test.cpp
@@ -3,5 +3,16 @@
 #include "nodecpp/moment.h"
 
 TEST_F(MomentTest, constructor) {
-  std::cout << "moment: " << moment().format() << std::endl;
+  cout << "moment: " << moment().format() << endl;
+  EXPECT_EQ(Moment(1u).valueOf(), 1000);
+  EXPECT_EQ(Moment(1u).unix(), 1);
+  EXPECT_EQ(Moment(1u).format(), "1970-01-01 08:00:01.000");
+
+  EXPECT_EQ(Moment(1ull).valueOf(), 1);
+  EXPECT_EQ(Moment(1ull).unix(), 0);
+  EXPECT_EQ(Moment(1000ull).unix(), 1);
+  EXPECT_EQ(Moment(1ull).format(), "1970-01-01 08:00:00.001");
+
+  EXPECT_EQ(Moment(1u, 1000000u).valueOf(), 1001);
+  EXPECT_EQ(Moment(1u, 1000000u).format(), "1970-01-01 08:00:01.001");
 }
