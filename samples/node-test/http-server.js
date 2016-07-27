644c9590ecf96da2f74d0589d20eb3e0007e360b
builden <billdeng@qq.com>
2016-07-27 20:34:34 +0800

    add http-proto test

--- samples/node-test/http-server.js
+++ samples/node-test/http-server.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const fs = require('fs-extra');
+const _ = require('lodash');
+
+const cfg = fs.readJsonSync('http-cfg.json');
+for (const key of _.keys(cfg)) {
+  cfg[key] = fs.readFileSync(cfg[key], 'utf8');
+}
+
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('X-Foo', 'bar');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('ok');
+  console.log(req.url);
+
+  if (cfg[req.url]) {
+    res.end(cfg[req.url]);
+  } else {
+    res.end('ok');
+  }
 });
 
 app.listen(3000);
